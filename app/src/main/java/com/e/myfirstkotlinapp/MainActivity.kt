91ea66ab0e8dce7ed0beffa22b4e2eb112a6092d
Malikovich <md.malikovich@gmail.com>
2020-02-26 21:25:52 +0600

    HomeWork #1 - almost done

---
+++ app/src/main/java/com/e/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.e.myfirstkotlinapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainClick()
+    }
+
+    val nameList = mutableListOf("name", "hello", "text")
+    val passList = mutableListOf("123", "123456")
+
+    private fun mainClick() {
+        click_btn.setOnClickListener {
+            val userName = username_txt.text.toString()
+            val password = password_txt.text.toString()
+            UIManager.showToast("$userName $password", this)
+            checkUserData(userName, password)
+        }
+    }
+
+    private fun checkUserData(userName: String, password: String) {
+        if (nameList.contains(userName) && passList.contains(password))
+            startActivity(
+                Intent(this, SecondActivity::class.java)
+                    .putExtra("userName", userName)
+                    .putExtra("password", password))
+        else UIManager.showToast("ERROR", this)
+    }
+}
---
+++ app/src/main/java/com/e/myfirstkotlinapp/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.e.myfirstkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val userName = intent.getStringExtra("userName")
+        val password = intent.getStringExtra("password")
+
+        val message = "userName: $userName \n password: $password"
+        text.text = message
+
+        UIManager.showToast("Hello its class from java", this)
+    }
+}
+
